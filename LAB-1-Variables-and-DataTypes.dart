d234e6380e1bf13adf9ef977d1e730529de116ae
Natan Amanuel Mamo <99745173+natanamanuel@users.noreply.github.com>
2024-03-08 18:26:36 +0300

    Add files via upload

---
+++ LAB-1-Variables-and-DataTypes.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Natan Amanuel";
+  int age = 22; // Replace with your actual age
+  String favoriteColor = "RED";
+
+  print(
+      "My name is $name, I am $age years old, and my favorite color is $favoriteColor.");
+}
